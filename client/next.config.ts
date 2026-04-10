fc12bfc804bf1a6e66ecd40c4c9824ddb30a6198
kirikalachev <146940419+kirikalachev@users.noreply.github.com>
2026-04-10 15:53:52 +0300

    .

--- client/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
--- server/project/medical_assistant/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedicalAssistantConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medical_assistant'
